08f1ea06628fc87a2d1c294138a57ec3386e3892
isfelaco <isfelaco@gmail.com>
2022-11-09 19:22:07 -0500

    sum_multiple_accum_C

--- looplab/sum_benchmarks.c
+++ looplab/sum_benchmarks.c
@@ -13,6 +13,16 @@ unsigned short sum_C(long size, unsigned short * a) {
     return sum;
 }
 
+unsigned short sum_multiple_accum_C(long size, unsigned short * a) {
+    unsigned short sum1 = 0;
+    unsigned short sum2 = 0;
+    for (int i = 0; i < size; i+=2) {
+        sum1 += a[i];
+        sum2 += a[i+1];
+    }
+    return sum1+sum2;
+}
+
 /* implementations in assembly */
 extern unsigned short sum_clang6_O(long, unsigned short *);
 extern unsigned short sum_gcc7_O3(long, unsigned short *);
@@ -25,11 +35,11 @@ extern unsigned short sum_multiple_accum(long, unsigned short *);
 /* This is the list of functions to test */
 function_info functions[] = {
     /* compiled versions from various compilers, each in their own .s file: */ 
-    // {sum_clang6_O, "sum_clang6_O: simple C compiled with clang 6 -O -mavx2"},
-    // {sum_gcc7_O3, "sum_gcc7_O3: simple C compiled with GCC7 -O3 -mavx2"}, 
+    {sum_clang6_O, "sum_clang6_O: simple C compiled with clang 6 -O -mavx2"},
+    {sum_gcc7_O3, "sum_gcc7_O3: simple C compiled with GCC7 -O3 -mavx2"}, 
 
     /* source code for this version is above */
-    // {sum_C, "sum_C: simple C compiled on this machine with settings in Makefile"},
+    {sum_C, "sum_C: simple C compiled on this machine with settings in Makefile"},
 
     /* source code for this version is in sum_simple.s */
     {sum_simple, "sum_simple: simple ASM implementation"},
@@ -38,6 +48,8 @@ function_info functions[] = {
     {sum_unrolled2, "sum_unrolled2"},
     {sum_unrolled4, "sum_unrolled4"},
     {sum_multiple_accum, "sum_multiple_accum"},
+    {sum_multiple_accum_C, "sum_multiple_accum_C"},
+
    
     {NULL, NULL},
 };
--- looplab/sum_benchmarks.s
+++ looplab/sum_benchmarks.s
@@ -27,31 +27,82 @@ sum_C:
 	.cfi_endproc
 .LFE5278:
 	.size	sum_C, .-sum_C
+	.p2align 4
+	.globl	sum_multiple_accum_C
+	.type	sum_multiple_accum_C, @function
+sum_multiple_accum_C:
+.LFB5279:
+	.cfi_startproc
+	endbr64
+	testq	%rdi, %rdi
+	jle	.L11
+	leaq	-2(%rdi,%rdi), %rax
+	xorl	%edx, %edx
+	andq	$-4, %rax
+	leaq	4(%rsi,%rax), %rcx
+	xorl	%eax, %eax
+	.p2align 4,,10
+	.p2align 3
+.L10:
+	addw	(%rsi), %ax
+	addw	2(%rsi), %dx
+	addq	$4, %rsi
+	cmpq	%rsi, %rcx
+	jne	.L10
+	addl	%edx, %eax
+	ret
+	.p2align 4,,10
+	.p2align 3
+.L11:
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE5279:
+	.size	sum_multiple_accum_C, .-sum_multiple_accum_C
 	.globl	functions
 	.section	.rodata.str1.8,"aMS",@progbits,1
 	.align 8
 .LC0:
+	.string	"sum_clang6_O: simple C compiled with clang 6 -O -mavx2"
+	.align 8
+.LC1:
+	.string	"sum_gcc7_O3: simple C compiled with GCC7 -O3 -mavx2"
+	.align 8
+.LC2:
+	.string	"sum_C: simple C compiled on this machine with settings in Makefile"
+	.align 8
+.LC3:
 	.string	"sum_simple: simple ASM implementation"
 	.section	.rodata.str1.1,"aMS",@progbits,1
-.LC1:
+.LC4:
 	.string	"sum_unrolled2"
-.LC2:
+.LC5:
 	.string	"sum_unrolled4"
-.LC3:
+.LC6:
 	.string	"sum_multiple_accum"
+.LC7:
+	.string	"sum_multiple_accum_C"
 	.section	.data.rel,"aw"
 	.align 32
 	.type	functions, @object
-	.size	functions, 80
+	.size	functions, 144
 functions:
-	.quad	sum_simple
+	.quad	sum_clang6_O
 	.quad	.LC0
-	.quad	sum_unrolled2
+	.quad	sum_gcc7_O3
 	.quad	.LC1
-	.quad	sum_unrolled4
+	.quad	sum_C
 	.quad	.LC2
-	.quad	sum_multiple_accum
+	.quad	sum_simple
 	.quad	.LC3
+	.quad	sum_unrolled2
+	.quad	.LC4
+	.quad	sum_unrolled4
+	.quad	.LC5
+	.quad	sum_multiple_accum
+	.quad	.LC6
+	.quad	sum_multiple_accum_C
+	.quad	.LC7
 	.quad	0
 	.quad	0
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
